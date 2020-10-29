756ee862a7afc504ab34a329f533e8428ae5832a
Jessica Kuksuk <jkuksuk@asu.edu>
2020-10-28 22:12:16 -0700

    Added response code 400

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -216,7 +216,10 @@ class WebServer {
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
-          response.sendError("Bad Request. Please look carefully at what and how many numbers you entered and try again.", 400);
+          builder.append("HTTP/1.1 400 Bad Request\n");
+          builder.append("Content-Type: text/html; charset=utf-8\n");
+          builder.append("\n");
+          builder.append("Please check your numbers and try again, sorry!");
 
         } else if (request.contains("github?")) {
           // pulls the query from the request and runs it with GitHub's REST API
